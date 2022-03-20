765521656267b4bfd09aaea01523719b30692862
MikeDoes <michaelam3099@gmail.com>
2022-03-20 23:56:59 +0100

    share host

--- annotator/run.sh
+++ annotator/run.sh
@@ -2,4 +2,4 @@ parentdir="$(dirname "$(pwd)")"
 dataset="/datasets/financial_alphabet_2021/"
 dataset_dir=$parentdir$dataset
 cp -r $dataset_dir unlabeled_data/financial_alphabet_2021/
-flask run
+flask run -h 0.0.0.0
