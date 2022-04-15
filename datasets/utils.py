a75c9f01225c299fabae05d27b224fefeba9df72
MikeDoes <michaelam3099@gmail.com>
2022-04-15 18:52:42 +0200

    correct file name

--- datasets/utils.py
+++ datasets/utils.py
@@ -3,7 +3,7 @@ def load_openie_dataset():
     import json
     import torch    
     # Loading the Open IE dataset into the PyTorch DataLoader
-    with open('datasets/oie2016/earnings_20161_openie5.json') as json_file:
+    with open('datasets/oie2016/data.json') as json_file:
         data = json.load(json_file)
 
     return data
